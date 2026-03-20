9f5aa6521cac04e067e6a2055b72788fe390d070
Ian Medina <ianmedina.can@gmail.com>
2026-03-20 12:07:58 -0600

    Start the data exploration searching for dtyps and null values

--- NFL_project.py
+++ NFL_project.py
@@ -9,4 +9,14 @@ warnings.filterwarnings('ignore')
 
 df = pd.read_csv("C:\\Users\\ianme\\Desktop\\NFL_Project\\team_stats_2003_2023.csv")
 
+# See data types and null values
 print(df.info())
+print("\n--- Null values ---")
+print(df.isnull().sum()[df.isnull().sum() > 0])
+
+# See duplicates
+print(f"\nDuplicados: {df.duplicated().sum()}")
+
+print(f"Unique Teams: {df['team'].nunique()}")
+print(f"Years covered: {df['year'].min()} - {df['year'].max()}")
+print(f"Register per year: {df.groupby('year').size().describe()}")
